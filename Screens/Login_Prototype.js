300806783d31029c56e3827086c04e1b60656b87
OXGN <cabarrubias.nevinharold@gmail.com>
2023-12-08 20:12:57 +0800

    Update Login_Prototype.js

--- Screens/Login_Prototype.js
+++ Screens/Login_Prototype.js
@@ -1,103 +1,118 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { IconButton, TextInput } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string().email('Invalid email address').required('Email is required'),
+        password: Yup.string().required('Password is required'),
+    });
 
-const Login = () => {
-    
+    const Login = () => {
     const navigation = useNavigation();
 
-    const forgotPass = () => {
-        console.log('forgot password is pressed');
-        navigation.navigate('AccountRecovery');
-    };
-    const gotoSignUp = () => {
-        navigation.navigate('Signup');
-    };
+    const handleLogin = async (values, { setSubmitting, setFieldError }) => {
+    try {
+        const response = await axios.post('http://192.168.1.15:3000/api/login', {
+            email: values.email,
+            password: values.password,
+        });
 
-    const gotoHome = () => {
-        navigation.navigate('Home');
-    };
-    
-return (
+        // Handle the response, store the token, and navigate to the next screen
+            console.log(response.data);
+        // Store the token (use AsyncStorage, Redux, or React Context) and navigate
+            navigation.navigate('Home');
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+        // Handle authentication failure
+        setFieldError('password', 'Invalid email or password');
+        } else {
+            // Handle other errors as needed
+        }
+    } finally {
+        setSubmitting(false);
+    }
+};
+
+    return (
     <SafeAreaView>
         <View style={styles.container}>
-            <Text style={styles.loginText}>
-                Welcome back!
-            </Text>
-            <TextInput
-                style={styles.loginTextInput}
-                placeholder='Email'
-                keyboardType='email-address'
-                autoCapitalize='none'
-            />
-            <TextInput
-                style={styles.loginTextInput}
-                placeholder='Password'
-                autoCapitalize='none'
-            />
-            <TouchableOpacity onPress={forgotPass}>
-                <Text style={styles.forgotpassButton}>
-                    Forgot Password
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.loginButton} onPress={gotoHome}>
-                <Text style={styles.loginTextButton}>
-                    Login
-                </Text>
-            </TouchableOpacity>
-            <View style={styles.signupWrapper}>
-                <Text style={styles.signupInfo}>
-                    Don't have an account? 
-                </Text>
-                <TouchableOpacity style={styles.signupButton} onPress={gotoSignUp}>
-                    <Text style={styles.signupText}>
-                        Sign up now!
-                    </Text>
+        <Text style={styles.loginText}>Welcome back!</Text>
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={LoginSchema}
+            onSubmit={handleLogin}
+        >
+            {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+            <View>
+                <TextInput
+                    style={styles.loginTextInput}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    value={values.email}
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                />
+                {touched.email && errors.email && (
+                <Text style={styles.errorText}>{errors.email}</Text>
+                )}
+
+                <TextInput
+                    style={styles.loginTextInput}
+                    placeholder="Password"
+                    autoCapitalize="none"
+                    secureTextEntry
+                    value={values.password}
+                    onChangeText={handleChange('password')}
+                    onBlur={handleBlur('password')}
+                />
+                {touched.password && errors.password && (
+                    <Text style={styles.errorText}>{errors.password}</Text>
+                )}
+
+                <TouchableOpacity
+                    style={styles.loginButton}
+                    onPress={handleSubmit}
+                    disabled={isSubmitting}
+                >
+                <Text style={styles.loginTextButton}>Login</Text>
                 </TouchableOpacity>
             </View>
+            )}
+        </Formik>
+        {/* ... */}
         </View>
     </SafeAreaView>
     )
-}
+};
 
 const styles = StyleSheet.create({
-    navigationBar:{
-        width: '100%',
-        height: '5%',
-        backgroundColor: '#ffffff',
-    },
-    container:{
+    container: {
         alignItems: 'center',
         justifyContent: 'center',
         height: '95%',
         width: 'auto',
         backgroundColor: '#ffffff',
     },
-    loginText:{
+    loginText: {
         fontSize: 40,
         width: '60%',
         textAlign: 'center',
         marginBottom: 20,
         fontWeight: 'bold',
     },
-    loginTextInput:{
+        loginTextInput: {
         fontSize: 20,
         width: '80%',
         margin: 10,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
     },
-    forgotpassButton:{
-        fontWeight: 'bold',
-        textAlign: 'right',
-        width: 340,
-        marginTop: 10,
-        textDecorationLine: 'underline',
-        fontSize: 15,
-    },
-    loginButton:{
+    loginButton: {
         width: 340,
         marginTop: 40,
         padding: 15,
@@ -105,29 +120,18 @@ const styles = StyleSheet.create({
         borderRadius: 25,
         elevation: 10,
     },
-    loginTextButton:{
+    loginTextButton: {
         color: 'white',
         textAlign: 'center',
         fontSize: 25,
         fontWeight: 'bold',
     },
-    signupWrapper:{
-        marginTop: 25,
-        flexDirection: 'row',
+    errorText: {
+        color: 'red',
+        marginTop: 10,
     },
-    signupInfo:{
-        marginRight: 3,
-        fontSize: 20,
-    },
-    signupButton:{
-
-    },
-    signupText:{
-        color: '#55bCF6',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-})
+});
+    
+    export default Login;
 // backgroundColor:'#E8EAED', // Gray
 // backgroundColor: '#55bCF6', // Blue
-export default Login;
